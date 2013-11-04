88cefe44b65979ce4298b1d2af6e76367a28d645
Jay <logikal@gmail.com>
2013-11-04 21:11:04 +1300

    moved to knockout - its what i know.

---
+++ js/grid.js
@@ -0,0 +1,58 @@
+$(function() {
+	var w = getWindowWidth(),
+		h = getWindowHeight(),
+		m = 25;
+		$g = $(".grid"),
+		$c = $(".col-13, .col-23");
+	
+	// set grid width & height to the window width and height - margin * 2
+	$g.width(w - (m * 2));
+	$g.height(h - (m * 2));
+	
+	// set any column heights to the window height - any header elements
+	$c.height(h - (m * 2) - $(".header").outerHeight(true));
+	
+	$(".action-required").show();
+});
+
+/**
+ * Cross Browser window height & width functions.
+ * from http://www.softcomplex.com/docs/get_window_size_and_scrollbar_position.html
+ *
+ * Copyright ©2002-2010 SoftComplex Inc. All rights reserved.
+ */
+function getWindowHeight() // viewport, not document
+{
+    var windowHeight = 0;
+    if (typeof(window.innerHeight) == 'number')
+    {
+        // DOM compliant, IE9+
+        windowHeight = window.innerHeight;
+    }
+    else
+    {
+        // IE6-8 workaround, Note: document can be smaller than window
+        var ieStrict = document.documentElement.clientHeight; // w/out DTD gives 0
+        var ieQuirks = document.body.clientHeight; // w/DTD gives document height
+        windowHeight = (ieStrict > 0) ? ieStrict : ieQuirks;
+    }
+    return windowHeight;
+}
+ 
+function getWindowWidth() // viewport, not document
+{
+    var windowWidth = 0;
+    if (typeof(window.innerWidth) == 'number')
+    {
+        // DOM compliant, IE9+
+        windowWidth = window.innerWidth;
+    }
+    else
+    {
+        // IE6-8 workaround, Note: document can be smaller than window
+        var ieStrict = document.documentElement.clientWidth; // w/out DTD gives 0
+        var ieQuirks = document.body.clientWidth; // w/DTD gives document width
+        windowWidth = (ieStrict > 0) ? ieStrict : ieQuirks;
+    }
+    return windowWidth;
+}
--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ $.ajaxPrefilter(function (options, originalOptions, jqXHR){
 	}
 	options.data = $.param($.extend(originalOptions.data, newObject));
 });
+
+function MainViewModel() {
+	var self = this;
+	self.containers = ko.observableArray([]);
+	self.files = ko.observable();
+	self.selectedContainer = ko.observable();
+	
+	self.getFiles = function() {
+		self.selectedContainer(this.id);
+		console.log(self.selectedContainer());
+	};
+}
+
+$(function() {
+	getContainers(function(result) {
+		var i = 1;
+		var viewModel = new MainViewModel();
+		var containers = result.containers;
+		_.each(containers, function(container) {
+			_.extend(container, { id: i });
+			viewModel.containers.push(container);
+			i++;
+		});
+
+		ko.applyBindings(viewModel);
+	});
+});
